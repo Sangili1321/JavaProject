c3ad8b66f39b62245260cafb1460b32f2aef60a1
Sangili1321 <114973971+Sangili1321@users.noreply.github.com>
2026-06-17 21:51:16 +0530

    Update DriverManager.java
    Created commit for fetch demo

--- src/main/java/driver/DriverManager.java
+++ src/main/java/driver/DriverManager.java
@@ -3,6 +3,7 @@ package driver;
 import org.openqa.selenium.WebDriver;
 
 //This class is to control the driver initialization
+//Added one more commit for fetch demo
 public final class DriverManager {
 
     private DriverManager() {}
